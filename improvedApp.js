fb1f104d57748fc196379b4ad466300a00206559
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-03-31 07:09:03 -0700

    Update index.html and set up model

---
+++ improvedApp.js
@@ -0,0 +1,37 @@
+/* ====== Model ===== */
+
+var model = {
+  currentCat: null,
+  cats: [
+    {
+      clickCount: 0,
+      name: 'Tabby',
+      imgSrc: 'img/cat_picture1.jpg',
+      imgAttribution: 'Tabby the Cat'
+    },
+    {
+      clickCount: 0,
+      name: 'Tiger',
+      imgSrc: 'img/cat_picture2.jpeg',
+      imgAttribution: 'Tiger the Cat'
+    },
+    {
+      clickCount: 0,
+      name: 'Scaredy',
+      imgSrc: 'img/cat_picture3.jpeg',
+      imgAttribution: 'Scaredy the Cat'
+    },
+    {
+      clickCount: 0,
+      name: 'Shadow',
+      imgSrc: 'img/cat_picture4.jpeg',
+      imgAttribution: 'Shadow the Cat'
+    },
+    {
+      clickCount: 0,
+      name: 'Sleepy',
+      imgSrc: 'img/cat_picture5.jpeg',
+      imgAttribution: 'Sleepy the Cat'
+    }
+  ]
+};
